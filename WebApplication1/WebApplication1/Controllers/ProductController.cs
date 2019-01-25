488e908d40a62bf92f9e59dbf237433803247d0a
Raina May <raina.may@yahoo.com>
2019-01-25 10:22:20 -0500

    Final Commit. 1/25/2019 10:22AM

---
+++ WebApplication1/WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public string Index()
+        {
+            return "Product/Index is displayed";
+        }
+        //
+        //GET: /Product/Browse
+        public string Browse()
+        {
+            return "Browse displayed";
+        }
+        //
+        //GET: /Product/Details/105
+        public string Details(int id)
+        {
+            string message = "Details displayed for id=" + id;
+
+            return message;
+        }
+        //
+        //GET: /Product/Location?zip=
+        public string Location(string zip)
+        {
+            string message =
+                HttpUtility.HtmlEncode("Location displayed for zip=" + zip);
+
+            return message;
+        }
+    }
+}
