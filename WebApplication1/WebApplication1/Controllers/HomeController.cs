048bb1d9f688974c5cf38d9fff4c48c2f035b68b
Raina May <raina.may@yahoo.com>
2019-02-10 13:58:01 -0500

    2/10/2019 1:57PM

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -10,7 +10,8 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View("~/Views/Other/Details.cshtml");
+            
         }
 
         public ActionResult About()
