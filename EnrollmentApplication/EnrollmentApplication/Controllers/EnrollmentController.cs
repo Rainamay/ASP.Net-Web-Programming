36bd9eaea2f49b1ada80ba01b06e58f6c39097c0
Raina May <raina.may@yahoo.com>
2019-03-25 02:25:27 -0400

    lab 9

--- EnrollmentApplication/EnrollmentApplication/Controllers/EnrollmentController.cs
+++ EnrollmentApplication/EnrollmentApplication/Controllers/EnrollmentController.cs
@@ -21,6 +21,32 @@ namespace EnrollmentApplication.Controllers
             return View(enrollments.ToList());
         }
 
+        public ActionResult StudentSearch(string q)
+        {
+            var students = GetStudents(q);
+            return PartialView("_StudentSearch",students);
+        }
+
+        private List<Student> GetStudents(string searchString)
+        {
+            return db.Students
+                .Where(a => a.FirstName.Contains(searchString) || a.LastName.Contains(searchString))
+                .ToList();
+        }
+
+        public ActionResult CourseSearch(string b)
+        {
+            var courses = GetCourses(b);
+            return PartialView("_CourseSearch", courses);
+        }
+
+        private List<Course> GetCourses(string searchString)
+        {
+            return db.Courses
+                .Where(a => a.Title.Contains(searchString) || a.Description.Contains(searchString))
+                .ToList();
+        }
+
         // GET: Enrollment/Details/5
         public ActionResult Details(int? id)
         {
