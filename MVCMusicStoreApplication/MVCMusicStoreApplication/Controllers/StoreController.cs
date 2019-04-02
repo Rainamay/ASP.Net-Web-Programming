03436ef2f940ee7b5f5c70b8a9e861166cdb0a16
Raina May <raina.may@yahoo.com>
2019-04-02 19:27:05 -0400

    Week 10

---
+++ MVCMusicStoreApplication/MVCMusicStoreApplication/Controllers/StoreController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCMusicStoreApplication.Models;
+
+namespace MVCMusicStoreApplication.Controllers
+{
+    public class StoreController : Controller
+    {
+        private MVCMusicStoreDB db = new MVCMusicStoreDB();
+
+        // GET: Store
+        public ActionResult Index(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var albums = GetAlbums(id); 
+            if (albums == null)
+            {
+                return HttpNotFound();
+            }
+            return View(albums);
+        }
+
+        private List<Album> GetAlbums(int? id)
+        {
+            return db.Albums.Where(a => a.GenreId == id).ToList();
+        }
+
+        public ActionResult Browse()
+        {
+            var genres = db.Genres;
+            return View(genres.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Album album = db.Albums.Find(id);
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            return View(album);
+        }
+    }
+}
